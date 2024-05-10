f1363028e9636db578f17fcc546cdc7326715435
Joel DIBASSO <joel@dibasso.fr>
2024-05-10 16:18:53 -0700

    Can make authenticated request to ICARE

--- screens/CheckQrCodeScreen.js
+++ screens/CheckQrCodeScreen.js
@@ -2,20 +2,24 @@ import { ActivityIndicator, View } from "react-native";
 import { router, useLocalSearchParams } from "expo-router";
 import { checkToken } from "../lib/auth";
 import { useEffect } from "react";
+import { useTheme } from "@react-navigation/native";
 
 export default function CheckQrCodeScreen() {
   const { token } = useLocalSearchParams();
+  const { colors } = useTheme();
+  console.log("Checking token", token);
   useEffect(() => {
     const stuff = () => {
       setTimeout(() => {
-        checkToken(token).then((isValid) => {
-          if (isValid) {
+        checkToken(token)
+          .then((userInfo) => {
+            console.log(userInfo);
             router.navigate("/login-ok");
-          } else {
-            // TODO: Make error screen
-          }
-        });
-      }, 2000);
+          })
+          .catch((error) => {
+            console.error("Invalid token", error);
+          });
+      }, 1000);
     };
     stuff();
   }, [token]);
@@ -31,7 +35,7 @@ export default function CheckQrCodeScreen() {
         alignItems: "center",
       }}
     >
-      <ActivityIndicator size="large" color="#457B9D" />
+      <ActivityIndicator size="large" color={colors.primary} />
     </View>
   );
 }
